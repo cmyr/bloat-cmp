6b6ba893b48acdad30c33c89f4b9172bdfcb4aee
Colin Rofls <colin@cmyr.net>
2019-10-02 23:56:26 -0400

    Add test workflow?

--- index.js
+++ index.js
@@ -11,12 +11,15 @@ try {
   const base_sizes = fs.readFileSync(base_sizes_path).toString();
   const new_sizes = fs.readFileSync(new_sizes_path).toString();
 
+  console.log('base: ' + base_sizes);
+  console.log('new: ' + new_sizes);
+
   const base_map = objify(base_sizes);
   const new_map = objify(new_sizes);
 
   for (var [key, new_size] of new_map) {
     var prev_size = base_map.get(key);
-    if prev_size === undefined {
+    if (prev_size === undefined) {
       console.log('item ' + key + ' does not exist in baseline.');
     } else {
       var diff = new_size - prev_size;
@@ -35,5 +38,5 @@ try {
 }
 
 function objify(string) {
-  return Map(string.split('\n').map(line => line.split(' ')))
+  return new Map(string.split('\n').map(line => line.split(' ')))
 }
