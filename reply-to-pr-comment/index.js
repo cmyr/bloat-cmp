7ff4e0baa7f4ad230cd456b03acdb07f74e3d612
Colin Rofls <colin@cmyr.net>
2019-10-04 14:17:13 -0400

    Handle more event types for commenting

--- reply-to-pr-comment/index.js
+++ reply-to-pr-comment/index.js
@@ -11,25 +11,60 @@ try {
     console.log(markdownText);
 
     const context = github.context;
+    console.log(context.event_name)
+    if (context.event_name = "push") {
+        postCommitComment(myToken, context, markdownText);
+    } else if (context.event_name = "pull_request") {
+        postPullRequestComment(myToken, context, markdownText);
+    } else if (context.event_name = "issue_comment") {
+        postIssueComment(myToken, context, markdownText);
+    } else {
+        core.setFailed('unhandled event type "' + context.event_name + '"');
+    }
 
     //const payload = JSON.stringify(context.payload, undefined, 2);
     //console.log(`The event payload: ${payload}`);
 
-    const postArgs = {
-        owner: context.payload.repository.owner.login,
-        repo: context.payload.repository.name,
-        issue_number: context.payload.issue.number,
-        body: markdownText,
-    };
-    let response = postComment(myToken, postArgs);
-    console.log(response);
+    //let response = postComment(myToken, postArgs);
+    //console.log(response);
 
 } catch (error) {
     core.setFailed(error.message);
 }
 
-async function postComment(token, postArgs) {
+async function postIssueComment(token, context, body) {
+    const postArgs = {
+        owner: context.payload.repository.owner.login,
+        repo: context.payload.repository.name,
+        issue_number: context.payload.issue.number,
+        body: body,
+    };
     const octokit = new github.GitHub(token, {log: console});
     var response = await octokit.issues.createComment(postArgs).then(ok => console.log('okay? ' + ok)).catch(err => console.log('error? :( ' + err));
+    return response;
+}
+
+async function postPullRequestComment(token, context, body) {
+    const postArgs = {
+        owner: context.payload.repository.owner.login,
+        repo: context.payload.repository.name,
+        issue_number: context.payload.number,
+        body: body,
+    };
+    const octokit = new github.GitHub(token, {log: console});
+    var response = await octokit.issues.createComment(postArgs).then(ok => console.log('okay? ' + ok)).catch(err => console.log('error? :( ' + err));
+    return response;
+}
+
+async function postCommitComment(token, context, body) {
+
+    const postArgs = {
+        owner: context.payload.repository.owner.login,
+        repo: context.payload.repository.name,
+        commit_sha: context.sha,
+        body: body,
+    };
+    const octokit = new github.GitHub(token, {log: console});
+    var response = await octokit.repos.createCommitComment(postArgs).then(ok => console.log('okay? ' + ok)).catch(err => console.log('error? :( ' + err));
     return response;
 }
