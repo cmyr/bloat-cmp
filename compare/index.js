c2df21aa3aab631d3242cd028cc80c1c9a5c45b6
Colin Rofls <colin@cmyr.net>
2019-10-04 16:45:09 -0400

    Fix missing rows

--- compare/index.js
+++ compare/index.js
@@ -1,16 +1,13 @@
 const core = require('@actions/core');
-const fs = require('fs');
 const bloatcmp = require('./bloat');
 
 try {
   const oldSizes = core.getInput('old');
   const newSizes = core.getInput('new');
-
   let oldObj = JSON.parse(oldSizes);
   let newObj = JSON.parse(newSizes);
 
   const result = JSON.stringify(bloatcmp.bloatcmp(oldObj, newObj));
-
   core.setOutput("stats", result);
 
 } catch (error) {
