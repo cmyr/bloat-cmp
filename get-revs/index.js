68bddcf0610557bbb5d456bc77b662d1f0c2b9ac
Colin Rofls <colin@cmyr.net>
2019-10-06 12:25:05 -0400

    Try to make get-revs work with one big fn

--- get-revs/index.js
+++ get-revs/index.js
@@ -3,22 +3,36 @@ const github = require('@actions/github');
 
 try {
     const context = github.context;
-    var base = "";
-    var head = ""
     switch (context.eventName) {
         case "pull_request":
-            base = context.payload.pull_request.base.ref;
-            head = `pull/${context.payload.pull_request.number}/merge`;
+            core.setOutput('base', context.payload.pull_request.base.ref);
+            core.setOutput(`pull/${context.payload.pull_request.number}/merge`);
             break;
         case "issue_comment":
-            let maybeRevs = maybeGetRevsFromComment(context.payload.issue.body);
+            console.log(`issue comment ${JSON.stringify(context.payload.comment.body)}`);
+            let maybeRevs = maybeGetRevsFromComment(context.payload.comment.body);
             if (maybeRevs != null) {
                 console.log(`using manual revs: ${maybeRevs}`);
-                head = maybeRevs[0];
-                base = maybeRevs[1];
+                core.setOutput('base', maybeRevs[0]);
+                core.setOutput('head', maybeRevs[1]);
             } else if ('pull_request' in context.payload.issue) {
-                head = `pull/${context.payload.issue.number}/merge`;
-                base = getBaseRev(context.payload.issue.number);
+
+                const myToken = core.getInput('myToken');
+                const octokit = new github.GitHub(myToken, { log: console });
+                octokit.pulls.get({
+                    owner: context.payload.repository.owner.login,
+                    repo: context.payload.repository.name,
+                    pull_number: context.payload.issue.number,
+                }).then(({ data }) => {
+                    let head = `pull/${data.number}/merge`;
+                    let base = data.base.ref;
+                    console.log(`got head '${head}' base '${base}'`);
+                    core.setOutput('base', base);
+                    core.setOutput('head', head);
+                }).catch(({ err }) => {
+                    console.log(`ERR: ${err}`);
+                    core.setFailed(err);
+                });
             } else {
                 core.setFailed('ignoring non PR comment');
             }
@@ -27,28 +41,25 @@ try {
             core.setFailed(`Unhandled event type ${context.eventName}`);
             return;
     }
-
-    core.setOutput('base', base);
-    core.setOutput('head', head);
-
 } catch (error) {
     core.setFailed(error.message);
 }
 
-async function getBaseRev(pull_number) {
-  try {
-    const myToken = core.getInput('myToken');
-    const octokit = new github.GitHub(myToken, { log: console });
-    const { data: pullRequest } = await octokit.pulls.get({ owner: github.context.payload.repository.owner.login, repo: github.context.payload.repository.name, pull_number });
-    return pullRequest.base.ref;
-  } catch (e) {
-    console.log(e);
-    throw e;
-  }
-}
+//async function getBaseRev(pull_number) {
+//try {
+//const myToken = core.getInput('myToken');
+//const octokit = new github.GitHub(myToken, { log: console });
+//const { data: pullRequest } = await octokit.pulls.get({ owner: github.context.payload.repository.owner.login, repo: github.context.payload.repository.name, pull_number });
+//return pullRequest.base.ref;
+//} catch (e) {
+//console.log(e);
+//throw e;
+//}
+//}
 
 // we accept comments in the form of '/bloat master..438afbd
 function maybeGetRevsFromComment(comment) {
+    console.log(`trying to get revs from comment '${comment}'`);
     let words = comment.split(' ');
     if (words.length == 3) {
         return [words[1], words[2]];
